e4ed3a4c5a16d4a5cac9f57a7d4d56287fad8f91
Pavel <paveln0366@gmail.com>
2021-02-07 16:17:56 +0300

    Received a list of movies from the site

--- app/src/main/java/com/pavelpotapov/filmoteka/MainActivity.java
+++ app/src/main/java/com/pavelpotapov/filmoteka/MainActivity.java
@@ -3,6 +3,16 @@ package com.pavelpotapov.filmoteka;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.pavelpotapov.filmoteka.data.Movie;
+import com.pavelpotapov.filmoteka.utils.JSONUtils;
+import com.pavelpotapov.filmoteka.utils.NetworkUtils;
+
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
