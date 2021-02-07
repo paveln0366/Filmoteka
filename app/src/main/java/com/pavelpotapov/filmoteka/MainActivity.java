078024e5da7302a524c97977b28a888e0dd636f0
Pavel <paveln0366@gmail.com>
2021-02-07 18:04:46 +0300

    Movie posters are displayed in a RecyclerView

--- app/src/main/java/com/pavelpotapov/filmoteka/MainActivity.java
+++ app/src/main/java/com/pavelpotapov/filmoteka/MainActivity.java
@@ -1,6 +1,8 @@
 package com.pavelpotapov.filmoteka;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
@@ -16,9 +18,19 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView recyclerViewPosters;
+    private MovieAdapter movieAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerViewPosters = findViewById(R.id.recyclerViewPosters);
+        recyclerViewPosters.setLayoutManager(new GridLayoutManager(this, 2));
+        movieAdapter = new MovieAdapter();
+        JSONObject jsonObject = NetworkUtils.getJSONFromNetwork(NetworkUtils.POPULARITY, 1);
+        ArrayList<Movie> movies = JSONUtils.getMoviesFromJSON(jsonObject);
+        movieAdapter.setMovies(movies);
+        recyclerViewPosters.setAdapter(movieAdapter);
     }
 }
---
+++ app/src/main/java/com/pavelpotapov/filmoteka/MovieAdapter.java
@@ -0,0 +1,67 @@
+package com.pavelpotapov.filmoteka;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.pavelpotapov.filmoteka.data.Movie;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHolder> {
+
+    private ArrayList<Movie> movies;
+
+    public MovieAdapter() {
+        movies = new ArrayList<>();
+    }
+
+    @NonNull
+    @Override
+    public MovieViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.movie_item, viewGroup, false);
+        return new MovieViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MovieViewHolder holder, int position) {
+        Movie movie = movies.get(position);
+//        ImageView imageView = holder.imageViewSmallPoster;
+//        Picasso.get().load(movie.getPosterPath()).into(imageView);
+        Picasso.get().load(movie.getPosterPath()).into(holder.imageViewSmallPoster);
+    }
+
+    @Override
+    public int getItemCount() {
+        return movies.size();
+    }
+
+    class MovieViewHolder extends RecyclerView.ViewHolder {
+
+        private ImageView imageViewSmallPoster;
+
+        public MovieViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageViewSmallPoster = itemView.findViewById(R.id.imageViewSmallPoster);
+        }
+    }
+
+    public void setMovies(ArrayList<Movie> movies) {
+        this.movies = movies;
+        notifyDataSetChanged();
+    }
+
+    public void addMovies(ArrayList<Movie> movies) {
+        this.movies.addAll(movies);
+        notifyDataSetChanged();
+    }
+
+    public ArrayList<Movie> getMovies() {
+        return movies;
+    }
+}
