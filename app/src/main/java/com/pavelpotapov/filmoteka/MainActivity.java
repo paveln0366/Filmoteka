515551732c27a9803fbd3844fca940ac3006e1a3
Pavel <paveln0366@gmail.com>
2021-02-08 16:38:41 +0300

    Downloaded data is saved to the database

--- app/src/main/java/com/pavelpotapov/filmoteka/MainActivity.java
+++ app/src/main/java/com/pavelpotapov/filmoteka/MainActivity.java
@@ -1,6 +1,10 @@
 package com.pavelpotapov.filmoteka;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -12,6 +16,7 @@ import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.pavelpotapov.filmoteka.data.MainViewModel;
 import com.pavelpotapov.filmoteka.data.Movie;
 import com.pavelpotapov.filmoteka.utils.JSONUtils;
 import com.pavelpotapov.filmoteka.utils.NetworkUtils;
@@ -19,6 +24,7 @@ import com.pavelpotapov.filmoteka.utils.NetworkUtils;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,10 +34,13 @@ public class MainActivity extends AppCompatActivity {
     private TextView textViewTopRated;
     private TextView textViewPopularity;
 
+    private MainViewModel viewModel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        viewModel = ViewModelProviders.of(this).get(MainViewModel.class);
         switchSort = findViewById(R.id.switchSort);
         textViewTopRated = findViewById(R.id.textViewTopRated);
         textViewPopularity = findViewById(R.id.textViewPopularity);
@@ -59,6 +68,13 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "End of list", Toast.LENGTH_SHORT).show();
             }
         });
+        LiveData<List<Movie>> moviesFromLiveData = viewModel.getMovies();
+        moviesFromLiveData.observe(this, new Observer<List<Movie>>() {
+            @Override
+            public void onChanged(List<Movie> movies) {
+                movieAdapter.setMovies(movies);
+            }
+        });
     }
 
     public void onClickSetPopularity(View view) {
@@ -82,8 +98,17 @@ public class MainActivity extends AppCompatActivity {
             textViewTopRated.setTextColor(getResources().getColor(R.color.white));
             methodOfSort = NetworkUtils.POPULARITY;
         }
+        downloadData(methodOfSort, 1);
+    }
+
+    private void downloadData(int methodOfSort, int page) {
         JSONObject jsonObject = NetworkUtils.getJSONFromNetwork(methodOfSort, 1);
         ArrayList<Movie> movies = JSONUtils.getMoviesFromJSON(jsonObject);
-        movieAdapter.setMovies(movies);
+        if (movies != null && !movies.isEmpty()) {
+            viewModel.deleteAllMovies();
+            for (Movie movie : movies) {
+                viewModel.insertMovie(movie);
+            }
+        }
     }
 }
--- app/src/main/java/com/pavelpotapov/filmoteka/MovieAdapter.java
+++ app/src/main/java/com/pavelpotapov/filmoteka/MovieAdapter.java
@@ -12,10 +12,11 @@ import com.pavelpotapov.filmoteka.data.Movie;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHolder> {
 
-    private ArrayList<Movie> movies;
+    private List<Movie> movies;
     private OnPosterClickListener onPosterClickListener;
     private OnRichEndListener onRichEndListener;
 
@@ -80,17 +81,17 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHol
         }
     }
 
-    public void setMovies(ArrayList<Movie> movies) {
+    public void setMovies(List<Movie> movies) {
         this.movies = movies;
         notifyDataSetChanged();
     }
 
-    public void addMovies(ArrayList<Movie> movies) {
+    public void addMovies(List<Movie> movies) {
         this.movies.addAll(movies);
         notifyDataSetChanged();
     }
 
-    public ArrayList<Movie> getMovies() {
+    public List<Movie> getMovies() {
         return movies;
     }
 }
