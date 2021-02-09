e3b13ac53249e812d1e976f5505b24452755c8ce
Pavel <paveln0366@gmail.com>
2021-02-09 15:26:50 +0300

    Replaced the method of loading movies

--- app/src/main/java/com/pavelpotapov/filmoteka/data/FavoriteMovie.java
+++ app/src/main/java/com/pavelpotapov/filmoteka/data/FavoriteMovie.java
@@ -5,12 +5,12 @@ import androidx.room.Ignore;
 
 @Entity(tableName = "favorite_movies")
 public class FavoriteMovie extends Movie {
-    public FavoriteMovie(int id, int voteCount, String title, String originalTitle, String overview, String posterPath, String bigPosterPath, String backdropPath, double voteAverage, String releaseDate) {
-        super(id, voteCount, title, originalTitle, overview, posterPath, bigPosterPath, backdropPath, voteAverage, releaseDate);
+    public FavoriteMovie(int uniqueId, int id, int voteCount, String title, String originalTitle, String overview, String posterPath, String bigPosterPath, String backdropPath, double voteAverage, String releaseDate) {
+        super(uniqueId, id, voteCount, title, originalTitle, overview, posterPath, bigPosterPath, backdropPath, voteAverage, releaseDate);
     }
 
     @Ignore
     public FavoriteMovie(Movie movie) {
-        super(movie.getId(), movie.getVoteCount(), movie.getTitle(), movie.getOriginalTitle(), movie.getOverview(), movie.getPosterPath(), movie.getBigPosterPath(), movie.getBackdropPath(), movie.getVoteAverage(), movie.getReleaseDate());
+        super(movie.getUniqueId(), movie.getId(), movie.getVoteCount(), movie.getTitle(), movie.getOriginalTitle(), movie.getOverview(), movie.getPosterPath(), movie.getBigPosterPath(), movie.getBackdropPath(), movie.getVoteAverage(), movie.getReleaseDate());
     }
 }
